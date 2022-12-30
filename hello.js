c6c9b2d2566df7ee557956751dea33bd5c9ca893
Liz Fosdick <lizfosdick@gmail.com>
2022-12-30 15:56:27 -0800

    Updated functions

--- hello.js
+++ hello.js
@@ -2,3 +2,8 @@ const sayHello = function (name) {
   console.log("Hello, " + name);
 }
 sayHello("Petey");
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
